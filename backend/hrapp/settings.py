22528ad3536c682c9433a38169b5e6191aa14fc0
abhishek <abhi.sol@outlook>
2025-06-17 04:08:11 +0530

    allowed hosts changed to render url removed http from url

--- backend/hrapp/settings.py
+++ backend/hrapp/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://basic-hrm.onrender.com']
+ALLOWED_HOSTS = ['basic-hrm.onrender.com','localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
